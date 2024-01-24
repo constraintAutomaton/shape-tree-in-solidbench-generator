b3148cdda9b186d935ed0bc07b51c071107bc972
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-01-24 14:03:46 +0100

    shape folder made optional

--- lib/util.ts
+++ lib/util.ts
@@ -13,7 +13,7 @@ export interface ShapeContentPath {
 
 export interface Config {
     pods_folder: string,
-    shape_folders:string|undefined,
+    shape_folders?: string,
     generate_shape: (path: string, shape_folder: string | undefined) => string | ShapeDontExistError,
     generate_shape_trees: (shapes: Array<ShapeContentPath>, pod_path: string) => void
 }
