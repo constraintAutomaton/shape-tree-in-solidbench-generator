eb299c23a7fd6246aac876ca7631e754945a3e0b
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-01-24 10:25:59 +0100

    clean index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
-import { walkSolidPods } from "./lib/walker";
-import type { Config } from "./lib/util";
-import { generateShapeTreesFile } from "./lib/shapeTreesUtil";
-import { getShapeFromPath } from "./lib/shapeUtil"
+export * from "./lib/walker";
+export type * from "./lib/util";
+export * from "./lib/shapeTreesUtil";
+export * from "./lib/shapeUtil"
 
-const config: Config = {
-    pod_folder: "out-fragments copy/http/localhost_3000/pods",
-    generate_shape: getShapeFromPath,
-    generate_shape_trees: generateShapeTreesFile,
-};
-walkSolidPods(config)
